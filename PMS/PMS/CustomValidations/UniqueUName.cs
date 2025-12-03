7eae8373226b449116d88675c7e60ceaa29d0d75
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2025-12-03 12:08:24 +0600

    PMS

---
+++ PMS/PMS/CustomValidations/UniqueUName.cs
@@ -0,0 +1,29 @@
+﻿using PMS.EF;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace PMS.CustomValidations
+{
+    public class UniqueUName : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            PMS_Fall25_CEntities db = new PMS_Fall25_CEntities();
+            if (value != null)
+            {
+                var uname = (from c in db.Customers
+                             where c.Username.Equals(value.ToString())
+                             select c).SingleOrDefault();
+                if (uname == null)
+                {
+                    return ValidationResult.Success;
+                }
+                else return new ValidationResult("username Exists");
+            }
+            else  return new ValidationResult("Check Values");
+        }
+    }
+}
