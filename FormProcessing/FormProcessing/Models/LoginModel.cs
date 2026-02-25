b30bdb23c8b6ff238bfeb5c8ef8760f0852c08c9
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2026-02-25 11:49:35 +0600

    Form Processing

---
+++ FormProcessing/FormProcessing/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FormProcessing.Models
+{
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Please provide Username")]
+        [StringLength(20,ErrorMessage = "Username shouldn't exceed 20")]
+        public string UName { get; set; }
+        [Required]
+        [StringLength(20,MinimumLength = 8)]
+        public string Pass { get; set; }
+    }
+}
