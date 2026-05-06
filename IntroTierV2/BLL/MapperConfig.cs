bbdf9a527ddc2f633021bd5e6cf357151f1aec30
tanvir.ahmeed.aiub <tanvir.ahmeed.aiub@gmail.com>
2026-05-06 12:17:52 +0600

    Demo

---
+++ IntroTierV2/BLL/MapperConfig.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Tables;
+
+namespace BLL
+{
+    public class MapperConfig
+    {
+        static MapperConfiguration config = new MapperConfiguration(cfg => {
+            cfg.CreateMap<Department,DepartmentDTO>().ReverseMap();
+        });
+        public static Mapper GetMapper() {
+            return new Mapper(config);
+        }
+    }
+}
---
+++ IntroTierV2/DAL/EF/Tables/Department.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DAL.EF.Tables;
+
+public partial class Department
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; } = null!;
+}
